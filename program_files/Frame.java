79bfa364096e1b7a60021b7e03ef027ae0d4837b
Kayla Berkey <featheredplane@yahoo.com>
2019-11-14 11:10:28 -0500

    Query and bug fix.

--- program_files/Frame.java
+++ program_files/Frame.java
@@ -137,7 +137,7 @@ class Frame extends JFrame
 		setTitle("Falcons Sports Cars");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);        
-        setResizable(true);
+        setResizable(false);
     }
 
     public void connectToDatabase(){
@@ -166,8 +166,8 @@ class Frame extends JFrame
         
         if (loggedIn || connectionHandler.conn != null){    
             try{
-                connectionHandler.closeConnection();
-                connectionHandler.resetConnectionProperties();
+                // connectionHandler.closeConnection();
+                // connectionHandler.resetConnectionProperties();
             }
             
             catch (Exception e){
@@ -351,7 +351,7 @@ class Frame extends JFrame
             String query = "SELECT SEI.id, SEI.dealership_number, SEI.first_name, SEI.last_name, SEI.email, SEI.street, SEI.zip, SEI.phone_number, TDSE.test_drives, SSE.sales, CSE.commissions_total " +
                             "FROM sales_emps_info SEI, test_drives_per_sales_emp_current_year TDSE, sales_per_sales_emp_current_year SSE, commissions_per_sales_emp_current_year CSE " +
                             "WHERE SEI.id = TDSE.id AND SEI.id = SSE.id AND SEI.id = CSE.id " +
-                            "ORDER BY num_sold_vehicles DESC " +
+                            "ORDER BY SEI.last_name DESC " +
                             "LIMIT 0, 5";
             // order by sum of prices
             performQueryAndDisplayResults(query);
